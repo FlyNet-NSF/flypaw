b16aa5a655f78da39dfefc6c2526dfc1a3d29f99
Eric Adams <eadams@delmontsystems.com>
2022-02-15 17:26:34 -0500

    class structure for basestation agent also plan directory

--- basestation/basestationAgent/basestationAgent.py
+++ basestation/basestationAgent/basestationAgent.py
@@ -6,28 +6,70 @@ import sys
 #import aerpawlib
 from datetime import datetime
 
+class IperfInfo(object):
+    def __init__(self, ipaddr="192.168.126.2", port=5201, protocol="tcp", priority=0, mbps=0, rtt=0):
+        self.ipaddr = ipaddr #string server ip address
+        self.port = port #string server port address 
+        self.protocol = protocol #tcp, udp
+        self.priority = 1 #normalized float 0-1         
+        self.mbps = mbps #float, units mbps, representing throughput
+        self.rtt = rtt #float, units ms, representing latency
+
 class collectVideoInfo(object):
     def __init__(self, dataformat="jpgframes", duration=5, quality=100, priority = 1):
-        self.dataformat = dataformat
+        self.dataformat = dataformat #jpgframes, ffmpeg, etc
         self.duration = duration #units seconds
         self.quality = quality #arbitrary unit
-        self.priority = priority
-    
-    #@property
-    #def dataformat(self):
-        #what are you collecting?
-        #for starters, jpg frames. FFMPEG to follow shortly.
-    #    return self.dataformat
-    #def duration(self):
-        #how long to collect in seconds
-    #    return self.duration
-    #def quality(self):
-        #video port
-    #    return self.quality
-    #def priority(self):
-        #implement priority function
-    #    return self.priority
-    
+        self.priority = priority #normalized float 0-1
+
+class sendVideoInfo(object):
+    def __init__(self, dataformat="jpgframes", ipaddr="192.168.126.2", port="23000", priority=1):
+        self.dataformat = dataformat #jpgframes, ffmpeg, etc
+        self.ipaddr = ipaddr #string ip address
+        self.port = port #int port number 
+        self.priority = priority #normalized float 0-1
+
+class flightInfo(object):
+    def __init__(self):
+        """
+        coords : [float,float]--> [lon, lat]
+        altitude: float --> M AGL(?)
+        airspeed: float --> 
+        """
+        self.coords = [] #[lon, lat]
+        self.altitude = float #meters 
+        self.airspeed = float #airspeed 
+        self.groundspeed = float #groundspeed
+        self.priority = float #normalized float 0-1
+
+class missionInfo(object):
+    #we'll have to think this through for different mission types
+    def __init__(self):
+        self.defaultWaypoints = [] #planfile
+        self.missionType = str #videography, delivery, air taxi, etc.
+        self.missionLeader = str #basestation, drone, cloud, edge device(s)
+        self.priority = float #normalized float from 0-1
+        self.planfile = str #path to planfile optional 
+        
+class VehicleCommands(object):
+    def __init__(self):
+        self.commands = {}
+        self.commands['iperf'] = {} 
+        self.commands['sendVideo'] = {} 
+        self.commands['collectVideo'] = {}
+        self.commands['flight'] = {}
+        
+    def setIperfCommand(self, iperfObj):
+        self.commands['iperf'] = { "command" : "iperf", "protocol": iperfObj.protocol, "ipaddr": iperfObj.ipaddr, "port": iperfObj.port, "priority": iperfObj.priority } 
+    def setCollectVideoCommand(self, collectVideoObj):
+        self.commands['collectVideo'] = { "command" : "collectVideo", "dataformat" : collectVideoObj.dataformat, "duration": collectVideoObj.duration, "quality": collectVideoObj.quality, "priority": collectVideoObj.priority }
+    def setSendVideoCommand(self, sendVideoObj):
+        self.commands['sendVideo'] = { "command" : "sendVideo", "dataformat" : sendVideoObj.dataformat, "ipaddr": sendVideoObj.ipaddr, "port": sendVideoObj.port, "priority": sendVideoObj.priority  }
+    def setFlightCommand(self, flightObj):
+        self.commands['flight'] = { "command" : "flight", "destination" : flightObj.destination, "speed": flightObj.speed, "priority": flightObj.priority }
+    def setMissionCommand(self, missionObj):
+        self.commands['mission'] = { "command": "mission", "defaultWaypoints": missionObj.defaultWaypoints, "missionType": missionObj.missionType, "missionControl": missionObj.missionControl, "priority": missionObj.priority }
+        
 class FlyPawBasestationAgent(object):
     def __init__(self, ipaddr="192.168.116.2", port=20001, chunkSize=1024) :
         self.ipaddr = ipaddr
@@ -36,8 +78,22 @@ class FlyPawBasestationAgent(object):
         self.iperf3Agent = IperfInfo()
         self.videoTransferAgent = sendVideoInfo()
         self.videoCollectionAgent = collectVideoInfo()
+        self.flightInfo = flightInfo()
+        self.missions = []
         self.currentRequests = []
         self.vehicleCommands = VehicleCommands()
+        self.vehicleCommands.setIperfCommand(self.iperf3Agent)
+        self.vehicleCommands.setCollectVideoCommand(self.videoCollectionAgent)
+        self.vehicleCommands.setSendVideoCommand(self.videoTransferAgent)
+        #for mission data, we should probably be checking elsewhere... for now we'll just define a mission here:
+        mission = missionInfo()
+        mission.missionType = "videography"
+        mission.missionLeader = "basestation"
+        mission.priority = 1
+        mission.planfile = "./mission.plan"
+        mission.default_waypoints = getWaypointsFromPlanfile()
+        #mission.default_waypoints = []
+        self.missions.append({'missionType': mission.missionType, 'missionLeader': mission.missionLeader, 'default_waypoints': mission.default_waypoints, 'priority': mission.priority})
         
     def basestationDispatch(self):
         UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -66,7 +122,7 @@ class FlyPawBasestationAgent(object):
                 if msgType == "telemetry":
                     #do something with telemetry
                     #currentRequests = []
-                    self.currentRequests.append(self.vehicleCommands['commands']['iperf']) # iperf as default                  
+                    self.currentRequests.append(self.vehicleCommands.commands['iperf']) # iperf as default                  
                 elif msgType == "instructionRequest":
                     msgFromServer['requests'] = self.currentRequests
                     self.currentRequests = []
@@ -74,11 +130,13 @@ class FlyPawBasestationAgent(object):
                     self.iperf3Agent.mbps = clientMessage[msgType]['mbps']
                     self.iperf3Agent.rtt = clientMessage[msgType]['meanrtt']
                     #if latestBW > 10000:
-                    self.currentRequests.append(self.vehicleCommands['commands']['sendVideo'])
+                    self.currentRequests.append(self.vehicleCommands.commands['sendVideo'])
                     #else:
                     #    self.currentRequests.append(self.vehicleCommands['commands']['flight'])                    
                 elif msgType == "sendVideo":
-                    self.currentRequests.append(self.vehicleCommands['commands']['flight'])
+                    self.currentRequests.append(self.vehicleCommands.commands['flight'])
+                elif msgType == "mission":
+                    msgFromServer['missions'] = self.missions
                 try: 
                     serialMsgFromServer = pickle.dumps(msgFromServer)
                     UDPServerSocket.sendto(serialMsgFromServer, address)
@@ -87,41 +145,11 @@ class FlyPawBasestationAgent(object):
                 
             except pickle.UnpicklingError as upe:
                 print ("cannot decode message from drone: " + upe)
-
-    class IperfInfo(object):
-        def __init__(self, ipaddr="192.168.126.2", port=5201, protocol="tcp", priority=0, mbps=-1, rtt=-1):
-            #collection info
-            self.port = port
-            self.ipaddr = ipaddr
-            self.protocol = protocol
-            self.priority = 1
-            
-            #results/current knowledge 
-            self.mbps = mbps
-            self.rtt = rtt
-    
-    class sendVideoInfo(object):
-        def __init__(self, dataformat="jpgframes", ipaddr="192.168.126.2", port="23000", priority=1):
-            self.dataformat = dataformat
-            self.ipaddr = ipaddr
-            self.port = port
-            self.priority = priority
-
-    class collectVideoInfo(object):
-        def __init__(self, dataformat="jpgframes", duration=5, quality=100, priority = 1):
-            self.dataformat = dataformat
-            self.duration = duration #units seconds
-            self.quality = quality #arbitrary for now
-            self.priority = priority
-    
-    class VehicleCommands(object):
-        def __init__(self):
-            self.commands = {}
-            self.commands['iperf'] = { "command" : "iperf", "protocol": self.iperf3Agent.protocol, "ipaddr": self.iperf3Agent.ipaddr, "port": self.iperf3Agent.port, "priority": self.iperf3Agent.priority }
-            self.commands['sendVideo'] = { "command" : "sendVideo", "dataformat" : self.videoTransferAgent.dataformat, "ipaddr": self.videoTransferAgent.ipaddr, "port": self.videoTransferAgent.port, "priority": videoTransferAgent.priority  }
-            self.commands['collectVideo'] = { "command" : "collectVideo", "dataformat" : self.videoCollectionAgent.dataformat, "duration": videoCollectionAgent.duration, "quality": videoCollectionAgent.quality, "priority": videoCollectionAgent.priority }
         
 if __name__ == '__main__':
     FPBA = FlyPawBasestationAgent()
     FPBA.basestationDispatch()
 
+
+def getWaypointsFromPlanfile(filepath):
+    
