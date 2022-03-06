babae798f5801e8020068fcc16aa04454edcbd69
Eric Adams <eadams@delmontsystems.com>
2022-03-06 12:07:52 -0500

    added iperf3 apt install, run scripts for tcp and udp versions, and copying them over to the Traffic directory

--- basestation/experiment_install_scripts/bandwidth.sh
+++ basestation/experiment_install_scripts/bandwidth.sh
@@ -3,16 +3,25 @@
 #git may already be installed by default
 apt-get install -y git
 
+#we'll use iperf3 server on the command line rather than the python wrapped version for now
+apt-get install -y iperf3
+
+#install python3.8 if it is not installed.
+apt-get install -y python3.8
+
 #as root, in /root directory, per the aerpaw standard experiment instruction
 git clone https://github.com/CASAelyons/flypaw.git
-
-#install python3.8 if it is not installed... Not going to script this for now
-#
 
 #copy run scripts to proper locations
 cp /root/flypaw/basestation/experiment_run_scripts/bandwidth/startBandwidthExperiment.sh /root
 
 cp /root/flypaw/basestation/experiment_run_scripts/bandwidth/startBasestationAgent.sh /root/Profiles/ProfileScripts/Vehicle
+
+#for tcp iperf3 server
+cp /root/flypaw/basestation/experiment_run_scripts/bandwidth/startIperf3Server_tcp.sh /root/Profiles/ProfileScripts/Traffic/startTraffic.sh
+
+#for udp iperf3 server
+#cp /root/flypaw/basestation/experiment_run_scripts/bandwidth/startIperf3Server_udp.sh /root/Profiles/ProfileScripts/Traffic/startTraffic.sh
 
 #copy planfile you want to use to mission.plan... maybe eventually just pass as argument to basestationAgent.py
 cp /root/flypaw/basestation/basestationAgent/plans/aerpaw_waypoint.plan /root/flypaw/basestation/basestationAgent/plans/mission.plan
